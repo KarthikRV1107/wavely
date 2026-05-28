f6b231352c21812ad4e205d18c7fc684bd056726
KARTHIK R V <karthikrv1107@gmail.com>
2026-05-28 13:18:53 +0530

    Removed files

--- src/utils/debounce.js
+++
@@ -1,7 +0,0 @@
-export const debounce = (fn, delay = 500) => {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
-  };
-};
--- src/utils/formatTime.js
+++
@@ -1,6 +0,0 @@
-export const formatTime = (totalSeconds) => {
-  if (!totalSeconds || isNaN(totalSeconds)) return '0:00';
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = Math.floor(totalSeconds % 60);
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-};
